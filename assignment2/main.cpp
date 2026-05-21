4893b117a22db6d4be7f6219cb64df3b998185eb
Sumanyu Aggarwal <sumanyu.code@gmail.com>
2026-05-21 15:57:35 +0530

    complete code

--- assignment2/main.cpp
+++ assignment2/main.cpp
@@ -52,8 +52,14 @@ template <typename T> struct ListNode {
  * @return A `unique_ptr` to the head of the list.
  */
 template <typename T> cs106l::unique_ptr<ListNode<T>> create_list(const std::vector<T>& values) {
-  /* STUDENT TODO: Implement this method */
-  throw std::runtime_error("Not implemented: createList");
+  if (values.empty()) return nullptr;
+  cs106l::unique_ptr<ListNode<T>> head = nullptr;
+  for (int i = values.size()-1; i >= 0; i--) {
+    cs106l::unique_ptr<ListNode<T>> node{new ListNode<T>{values[i]}};
+    node->next = std::move(head);
+    head = std::move(node);
+  }
+  return head;
 }
 
 /**
