d61ba5c411269b3f243bdaca9dca73bb8a6cbe80
Sumanyu Aggarwal <sumanyu.code@gmail.com>
2026-05-28 17:13:33 +0530

    implement tokenize

--- assignment3-ispell-templates-concepts/spellcheck.cpp
+++ assignment3-ispell-templates-concepts/spellcheck.cpp
@@ -11,8 +11,13 @@ template <typename Iterator, typename UnaryPred>
 std::vector<Iterator> find_all(Iterator begin, Iterator end, UnaryPred pred);
 
 Corpus tokenize(std::string& source) {
-  /* TODO: Implement this method */
-  return Corpus();
+  auto spaces = find_all(source.begin(), source.end(), [](char c) { return std::isspace(c); });
+  Corpus tokens;
+  std::transform(spaces.begin(), spaces.end()-1, spaces.begin()+1, std::inserter(tokens, tokens.begin()), [&source](auto it1, auto it2) {
+    return Token{source, it1, it2};
+  });
+  std::erase_if(tokens, [](Token token) { return token.content.empty(); });
+  return tokens;
 }
 
 std::set<Misspelling> spellcheck(const Corpus& source, const Dictionary& dictionary) {
