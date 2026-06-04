9db55b6fe9893190350579fbdfe3fd54d86f18b7
Sumanyu Aggarwal <sumanyu.code@gmail.com>
2026-06-04 15:09:12 +0530

    Part 1 and 2

--- assignment5-treebook-operator-overload-dry/user.cpp
+++ assignment5-treebook-operator-overload-dry/user.cpp
@@ -59,7 +59,37 @@ void User::set_friend(size_t index, const std::string& name)
   _friends[index] = name;
 }
 
-/** 
- * STUDENT TODO:
- * The definitions for your custom operators and special member functions will go here!
- */
+std::ostream& operator<<(std::ostream& os, const User& user) {
+  os << "User(name=" << user._name << ", friends=[";
+  for (int i = 0; i < user._size; i++) {
+    os << user._friends[i];
+    if (i < user._size - 1) os << ", ";
+  }
+  os << "])";
+  return os;
+}
+
+User::~User() {
+  delete[] _friends;
+}
+
+User::User(const User& user): _name{user._name}, _friends{new std::string[user._capacity]},
+                              _size{user._size}, _capacity{user._capacity}
+{
+  for (int i = 0; i < _size; i++) {
+    _friends[i] = user._friends[i];
+  }
+}
+
+User& User::operator=(const User& user) {
+  if (this == &user) return *this;
+  delete[] _friends;
+  _name = user._name;
+  _friends = new std::string[user._capacity];
+  _size = user._size;
+  _capacity = user._capacity;
+  for (int i = 0; i < _size; i++) {
+    _friends[i] = user._friends[i];
+  }
+  return *this;
+}
--- assignment5-treebook-operator-overload-dry/user.h
+++ assignment5-treebook-operator-overload-dry/user.h
@@ -16,10 +16,13 @@ public:
   size_t size() const;
   void set_friend(size_t index, const std::string& name);
 
-  /** 
-   * STUDENT TODO:
-   * Your custom operators and special member functions will go here!
-   */
+  friend std::ostream& operator<<(std::ostream& os, const User& user);
+
+  ~User();
+  User(const User& user);
+  User& operator=(const User& user);
+  User(User&& user) = delete;
+  User& operator=(User&& user) = delete;
 
 private:
   std::string _name;
